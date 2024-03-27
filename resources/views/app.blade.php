86bc8908720f37fa020415df17de7836a94393b9
Amoseven <omsk.web@gmail.com>
2024-03-27 11:32:20 +0600

    init commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body>
+<div class="antialiased m-auto w-1/2 mt-20">
+    <h1 class="text-4xl font-bold text-blue-700">
+        Hello world, my Friend!
+    </h1>
+</div>
+</body>
+</html>
